95eb458f5ca5fbbdf5576e6c6c3369f2438f92e4
silatrommi <immortalisx1@gmail.com>
2025-11-13 19:21:24 +0900

    js-commit

--- js/character-count/character-count.js
+++ js/character-count/character-count.js
@@ -2,9 +2,11 @@ const input = document.getElementById("input");
 const countDisplay = document.getElementById("count");
 let timeoutId;
 
+const DELAY_ms = 1000;
+
 input.addEventListener("input", () => {
   clearTimeout(timeoutId);
   timeoutId = setTimeout(() => {
     countDisplay.textContent = `文字数：${input.value.length}`;
-  }, 1000);
+  }, DELAY_ms);
 });
--- js/clock/clock.js
+++ js/clock/clock.js
@@ -1,9 +1,13 @@
 const clock = document.getElementById("clock");
+const UPDATE_INTERVAL_ms = 100;
 
 const updateClock = () => {
   const now = new Date();
-  const formatted = `${now.getFullYear()}年${now.getMonth()+1}月${now.getDate()}日-${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${Math.floor(now.getMilliseconds()/100)}`;
+  const formatted = `${now.getFullYear()}年${now.getMonth() + 1}月${now.getDate()}日-${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${Math.floor(now.getMilliseconds() / 100)}`;
+  
   clock.textContent = formatted;
+  clock.setAttribute("datetime", now.toISOString());
 };
 
-setInterval(updateClock, 100);
+updateClock();
+setInterval(updateClock, UPDATE_INTERVAL_ms);
